0e4d240a70f0d85c86ddba3b1aeb132175b509bb
biti2008 <tesfaybrkti00@gmail.com>
2022-03-15 12:37:43 -0700

    My num ten commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -16,28 +18,29 @@
  * All your code should be in the main function
  * Return: 0
  */
-int main(void){
+int main(void)
+{
   int c;
   int d = 0;
   while (d < 10)
-  {
-        c = 0;
-        while (c < 10)
         {
-            if (d != c && d < c)
-            {
-                putchar('0' + d);
-                putchar('0' + c);
-                if (c + d != 17)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-            c++;
+            c = 0;
+                while (c < 10)
+                     {
+                        if (d != c && d < c)
+                            {
+                                putchar('0' + d);
+                                    putchar('0' + c);
+                                        if (c + d != 17)
+                                            {
+                                                putchar(',');
+ 				                       putchar(' ');
+                			    }
+                		}
+                		c++;
+            	}
+            	d++;
         }
-        d++;
-    }
   putchar('\n');
   return (0);
 }
