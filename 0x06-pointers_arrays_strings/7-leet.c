bfed93363659cd8dee41110006d1d0ded7379acc
biti2008 <tesfaybrkti00@gmail.com>
2022-03-24 07:44:46 -0430

    My num 7 commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,33 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
+ * cap_string - Function that reverses the content of an array of integers.
+ *
+ * @s: s is the array
+ *
+ * Return: Always 0.
  */
-
-char *rot13(char *s)
+char *cap_string(char *s)
 {
-  int i = 0;
+	int i = 0;
 
-  while (s[i] != '\0')
-  {
-    while ((s[i] >= 'a' && s[i] <= 'z') 
-        (s[i] >= 'A' && s[i] <= 'Z'))
-    {
-      if ((s[i] >= 'a' && s[i] <= 'm') 
-          (s[i] >= 'A' && s[i] <= 'M'))
-        s[i] += 13;
-      else
-        s[i] -= 13;
-      i++;
-    }
-    i++;
-  }
-  return (s);
+	while (s[i] != '\0')
+	{
+		if (s[0] <= 122 && s[0] >= 97)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
+			s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
+				s[i] == '!' || s[i] == '?' || s[i] == '(' ||
+				s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	i++;
+	}
+	return (s);
 }
