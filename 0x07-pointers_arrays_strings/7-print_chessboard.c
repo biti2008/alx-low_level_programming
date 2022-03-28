a2c1b61c771088dc560ecd97ad8cc85fd4b3aec2
biti2008 <tesfaybrkti00@gmail.com>
2022-03-28 08:57:54 -0430

    My commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,40 @@
-#include "main.h"
-#include <stdlib.h>
-/**
-  *print_chessboard - prints the chessboard.
-  *@a: array of pointers.
-  *
-  *Return: void.
-  */
-void print_chessboard(char (*a)[8])
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+void main(){
+int gr=DETECT,gm;
+int row,col,x=50,y=50,flag=0;
+initgraph(&gr,&gm,"C:\\TURBOC3\\BGI");
+printf("\t*********** CHESS BOARD **************\n");
+for(row=0;row<8;row++)
 {
-	int  i, j;
 
-	for (i = 0; i <= 7; i++)
-	{
-		for (j = 0; j <= 7; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar(10);
-	}
+for(col=1;col<=8;col++){
+if(flag==0){
+setcolor(YELLOW);
+setfillstyle(SOLID_FILL,BLACK);
+rectangle(x,y,x+50,y+50);
+floodfill(x+1,y+1,YELLOW);
+flag=1;
+}
+else
+{
+setcolor(YELLOW);
+setfillstyle(SOLID_FILL,WHITE);
+rectangle(x,y,x+50,y+50);
+floodfill(x+1,y+1,YELLOW);
+flag=0;
+}
+x=x+50;
+}
+if(flag==0)
+flag=1;
+else
+flag=0;
+delay(100);
+x=50;
+y=50+y;
+}
+getch();
+closegraph();
 }
