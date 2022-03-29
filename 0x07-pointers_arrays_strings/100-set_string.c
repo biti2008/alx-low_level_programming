5b4a1d49ae0642f95236f00036d4d9dd634b1eba
biti2008 <tesfaybrkti00@gmail.com>
2022-03-29 07:00:07 -0430

    My commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,9 @@
 #include "main.h"
+
 /**
- * set_string - sets the value of a pointer to a char
- *
- * @s: The pointer to change
- * @to: The value to change the pointer to
- *
- * Return: nothing
+ * set_string - sets the value of a pointer to a char.
+ * @s: puntero doble.
+ * @to: puntero.
  */
 
 void set_string(char **s, char *to)
