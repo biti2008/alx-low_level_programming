e82468ed282585fea0c70c777708a482e4d89071
Birktiy Tesfay <Birktiy.Tesfay@safaricom.et>
2022-11-16 23:09:22 -0800

    My commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,29 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * puts_half - prints half of a string, followed by a new line.
- * @str: the string to be manipulated.
- *
- * Return: nothing.
+ * puts_half - prints the second half of a string.
+ *@str: string to use.
  */
+
 void puts_half(char *str)
 {
-	int i = 0, j;
+	int length, n, i;
 
-	for (; str[i] != '\0'; i++)
-		;
-
-	if (i % 2)
+	for (length = 0; str[length] != '\0'; length++)
 	{
-		j = (i - 1) / 2;
-		for (; j < (i - 1); j++)
-			_putchar(str[j + 1]);
 	}
-	else
+	n = (length - 1) / 2;
+	for (i = n + 1; str[i] != '\0'; i++)
 	{
-		j = i / 2;
-		for (; j < i; j++)
-			_putchar(str[j]);
+		_putchar(str[i]);
 	}
-	_putchar(10);
-
+	_putchar('\n');
+}
